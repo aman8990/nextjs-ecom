aa788c6ebf3e36e10cbf47b71d03841fd3ff8274
aman8990 <amandalal899@gmail.com>
2025-07-06 01:13:39 +0530

    otp-api-change

--- app/api/send-forgot-otp/route.js
+++ app/api/send-forgot-otp/route.js
@@ -5,13 +5,13 @@ import fs from 'fs';
 import path from 'path';
 import { sendEmail } from '@/app/_libs/emailSender';
 
-const getOTPEmailTemplate = (otp, email) => {
-  const templatePath = path.join('app/_libs', 'forgotOtpTemplate.html');
-  let html = fs.readFileSync(templatePath, 'utf-8');
-  html = html.replace('{{otp}}', otp);
-  html = html.replace('{{email}}', email);
-  return html;
-};
+// const getOTPEmailTemplate = (otp, email) => {
+//   const templatePath = path.join('app/_libs', 'forgotOtpTemplate.html');
+//   let html = fs.readFileSync(templatePath, 'utf-8');
+//   html = html.replace('{{otp}}', otp);
+//   html = html.replace('{{email}}', email);
+//   return html;
+// };
 
 export async function POST(request) {
   try {
@@ -43,13 +43,13 @@ export async function POST(request) {
       },
     });
 
-    const htmlContent = getOTPEmailTemplate(otp, email);
+    // const htmlContent = getOTPEmailTemplate(otp, email);
 
     await sendEmail({
       to: email,
       subject: 'Forgot Password OTP',
       text: `Your OTP for Forgot Password is ${otp}. It is valid for 10 minutes.`,
-      html: htmlContent,
+      html: `<p>Your OTP for Forgot Password is <strong>${otp}</strong>. It is valid for 10 minutes.</p>`,
     });
 
     return NextResponse.json('OTP sent successfully', { status: 200 });
